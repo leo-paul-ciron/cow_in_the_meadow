c6b2e7a2cf80108044da01f011fa227cc6ac7271
Macrelle_T <65347962+FlowWars@users.noreply.github.com>
2021-05-01 21:42:29 +0200

    vérification sur la saisie des piquets

--- ProjetTutorerTest/Program.cs
+++ ProjetTutorerTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace ProjetTutorerTest
 {
@@ -7,16 +8,27 @@ namespace ProjetTutorerTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entrer le nombre de piquet");
+            Console.WriteLine("Entrer le nombre de piquet (minimum 3)");
             string nb_piquet_string = Console.ReadLine();
+            bool verif_caract = true;
+            do
+            {
+                Console.WriteLine("Veuillez insérez un nombre supérieur à 3 !");
+                nb_piquet_string = Console.ReadLine();
+                if (Regex.IsMatch(nb_piquet_string, @"^[0-9]+$") && int.Parse(nb_piquet_string) >= 3)
+                {
+                    verif_caract = false;
+                }
+            }
+            while (string.IsNullOrEmpty(nb_piquet_string) || verif_caract == true);
             int nb_piquet = int.Parse(nb_piquet_string);
-            Console.WriteLine("nombre de piquet {0}",nb_piquet);
+            Console.WriteLine("nombre de piquet : {0}",nb_piquet);
             Piquet[] tabPiquet = new Piquet[nb_piquet];
             for (int idx = 0; idx < nb_piquet; idx++)
             {
-                Console.WriteLine("entrer l'abscise du piquet {0}", idx + 1);
+                Console.WriteLine("entrer l'abscise du piquet : {0}", idx + 1);
                 double v_abscise = double.Parse(Console.ReadLine());
-                Console.WriteLine("entrer l'ordonnée du piquet {0}", idx + 1);
+                Console.WriteLine("entrer l'ordonnée du piquet : {0}", idx + 1);
                 double v_ordonner = double.Parse(Console.ReadLine());
 
                 tabPiquet[idx] = new Piquet(v_abscise, v_ordonner, idx+1);
