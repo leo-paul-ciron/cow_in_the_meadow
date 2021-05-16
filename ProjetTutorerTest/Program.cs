3824a13c7ee9ea161a6b2508a324290ace8377cd
leo-paul-ciron <73255341+leo-paul-ciron@users.noreply.github.com>
2021-05-16 22:55:47 +0200

    ajout des vérifications de saisie

--- ProjetTutorerTest/Program.cs
+++ ProjetTutorerTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace ProjetTutorerTest
 {
@@ -7,18 +8,69 @@ namespace ProjetTutorerTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entrer le nombre de piquet ");
+            bool verif_caract = true;
+            Console.WriteLine("Entrer le nombre de piquet (minimum 3)");
 
             string nb_piquet_string = Console.ReadLine();
-            int nb_piquet = int.Parse(nb_piquet_string);
-            Console.WriteLine("nombre de piquet {0}",nb_piquet);
+            int nb_piquet = 0;
+            if (Regex.IsMatch(nb_piquet_string, @"^[0-9]+$") && int.Parse(nb_piquet_string) >= 3)
+            {
+                nb_piquet = int.Parse(nb_piquet_string);
+            }
+            else
+            {
+                do
+                {
+                    Console.WriteLine("Veuillez insérez un nombre supérieur à 3 !");
+                    nb_piquet_string = Console.ReadLine();
+                    if (Regex.IsMatch(nb_piquet_string, @"^[0-9]+$") && int.Parse(nb_piquet_string) >= 3)
+                    {
+                        nb_piquet = int.Parse(nb_piquet_string);
+                        verif_caract = false;
+                    }
+
+                }
+                while (string.IsNullOrEmpty(nb_piquet_string) || verif_caract == true);
+            }
+            Console.WriteLine("Nombre de piquet : {0}", nb_piquet);
             Piquet[] tabPiquet = new Piquet[nb_piquet];
             for (int idx = 0; idx < nb_piquet; idx++)
             {
-                Console.WriteLine("entrer l'abscise du piquet {0}", idx + 1);
-                double v_abscise = double.Parse(Console.ReadLine());
-                Console.WriteLine("entrer l'ordonnée du piquet {0}", idx + 1);
-                double v_ordonner = double.Parse(Console.ReadLine());
+                Console.WriteLine("Entrer l'abscisse du piquet : {0}", idx + 1);
+                double v_abscise = 0;
+                double v_ordonner = 0;
+                bool erreur = false;
+                do
+                {
+                    try
+                    {
+
+                        v_abscise = double.Parse(Console.ReadLine());
+                        erreur = false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Veuillez saisir un nombre !");
+                        erreur = true;
+                    }
+
+                } while (erreur == true);
+                Console.WriteLine("Entrer l'ordonnée du piquet : {0}", idx + 1);
+                do
+                {
+                    try
+                    {
+
+                        v_ordonner = double.Parse(Console.ReadLine());
+                        erreur = false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Veuillez saisir un nombre !");
+                        erreur = true;
+                    }
+
+                } while (erreur == true);
 
                 tabPiquet[idx] = new Piquet(v_abscise, v_ordonner, idx+1);
             }
